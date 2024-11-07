c18a6989fce8e1bccd1570853ec89d435fdc79dc
Ben Gitter <gitterbd@gmail.com>
2024-11-07 02:45:01 +0000

    added function to drop schema, useful while testings, probably shouldnt go into prod.

--- create_schema.py
+++ create_schema.py
@@ -1,9 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy_utils import database_exists, create_database
-from lmos_database_schema import Base
+from lmos_database_schema.tables import Base
 
 # Database connection URL
-DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/lmos"
+DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/test_db"
 
 def create_schema():
     # Create the engine
@@ -12,13 +12,22 @@ def create_schema():
     # Create database if it doesn't exist
     if not database_exists(engine.url):
         create_database(engine.url)
-        print(f"Created database 'lmos'")
+        print(f"Created database 'test_db'")
     else:
-        print(f"Database 'lmos' already exists")
+        print(f"Database 'test_db' already exists")
 
     # Create all tables
     Base.metadata.create_all(engine)
     print("Created all database tables")
 
+def drop_schema():
+    # Create the engine
+    engine = create_engine(DATABASE_URL)
+    
+    # Drop all tables
+    Base.metadata.drop_all(engine)
+    print("Dropped all database tables")
+
 if __name__ == "__main__":
+    #drop_schema()
     create_schema()
